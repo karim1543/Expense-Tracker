55df205e8656f0aaa3137e85b049f3190fb355ff
Karim Sayed <karimsayed156@outlook.com>
2025-05-21 20:40:35 +0300

    added validation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+const { body } = require('express-validator');
 
-router.post('/register', registerUser);
+const { registerUser, loginUser } = require('../controllers/authController');
+const User = require('../models/User');
+
+router.post('/register', [
+     body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then(userDoc => {
+          if (userDoc) {
+            return Promise.reject('E-Mail address already exists!');
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+], registerUser);
 router.post('/login', loginUser);
 
 module.exports = router;
